6faf1fc046a39fad362886d502a219400036d03f
Irem Gunay <iremguunay@gmail.com>
2022-09-15 18:55:49 +0300

    variables and data types examples

---
+++ 01-week-assignments/variables/src/com/iremgunay/Main.java
@@ -0,0 +1,15 @@
+package com.iremgunay;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int ogrenciSayisi = 12;
+        String mesaj = "Ogrenci sayısı: ";
+
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println("Öğrenci sayısı: " + ogrenciSayisi);
+        System.out.println("Öğrenci sayısı: " + ogrenciSayisi);
+    }
+}
