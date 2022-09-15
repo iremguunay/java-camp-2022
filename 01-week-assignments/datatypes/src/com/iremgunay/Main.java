bed998f0c57ab00063137e9bd38729889b8c119d
Irem Gunay <iremguunay@gmail.com>
2022-09-15 19:09:11 +0300

    variables and data types examples

---
+++ 01-week-assignments/datatypes/src/com/iremgunay/Main.java
@@ -0,0 +1,22 @@
+package com.iremgunay;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        byte sayi = 12;
+        sayi = 127; // byte değeri 127'den büyük olamaz
+
+        int sayi2 = 12;
+        sayi2 = 129;
+
+        double sayi3 = 12.5;
+        sayi3 = -129;
+
+        String sehir = "Ankara";
+        char karakter = 'A'; // tek tirnak
+
+        boolean dogruMu = false;
+
+    }
+}
