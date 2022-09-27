407b4069654ed095f1ed7ad9af54143a3f4efed7
Irem Gunay <iremguunay@gmail.com>
2022-09-27 18:22:02 +0300

    four operations example using classes

---
+++ 02-week-assignments/reCapDemo_Classes/src/com/iremgunay/Main.java
@@ -0,0 +1,10 @@
+package com.iremgunay;
+
+public class Main {
+
+    public static void main(String[] args) {
+        FourOperations fourOperations = new FourOperations();
+        int result = fourOperations.addition(3,4);
+        System.out.println(result);
+    }
+}
