17f02cb58d7f941c070c8185ef0fe7ad4337b8b3
Irem Gunay <iremguunay@gmail.com>
2022-09-27 18:14:10 +0300

    understand difference between value type and reference type

--- 02-week-assignments/classes/src/com/iremgunay/Main.java
+++ 02-week-assignments/classes/src/com/iremgunay/Main.java
@@ -13,6 +13,19 @@ public class Main {
         customerManager.remove();
         customerManager.update();
 
+        // value type
+        int number1 = 10;
+        int number2 = 20;
+        number2 = number1;
+        number1 = 30;
+        System.out.println(number2); // result : 10
+
+        // reference type
+        int[] numbers1 = new int[]{1, 2, 3};
+        int[] numbers2 = new int[]{4, 5, 6};
+        numbers2 = numbers1;
+        numbers1[0] = 10;
+        System.out.println(numbers2[0]); // result : 10
 
 
 
